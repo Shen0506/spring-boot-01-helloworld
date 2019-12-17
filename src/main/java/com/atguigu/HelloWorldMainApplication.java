cc99c7ab4e6ce54ef6e1790817400ae778dbbfab
Shen0506 <53997621+Shen0506@users.noreply.github.com>
2019-12-17 15:24:52 +0800

    Update HelloWorldMainApplication.java

--- src/main/java/com/atguigu/HelloWorldMainApplication.java
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
+ *修改1
  */
 @SpringBootApplication
 public class HelloWorldMainApplication {
